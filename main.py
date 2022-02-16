1cd1d5f69a0c18b7814cfdb716b727e45c38217e
Tiago <78756825+Tiago08@users.noreply.github.com>
2022-02-16 11:57:24 -0400

    Finished the project

--- main.py
+++ main.py
@@ -1,41 +1,75 @@
-# set the characters for the password
-# use random for generate the random password
-# use tkinter for create the GUI
 import random
-import os
-import sys
+import pyperclip
+from tkinter import *
+from tkinter.ttk import *
 
-class pass_generator():
-    def __init__(self):
-        """Setting the number of characters"""
-        while True:
-            try:
-                self.length = int(
-                    input('Length of the Password (min 8):'))
-                if self.length < 8:
-                    print("Write a number higher than 7")
-                else:
-                    break
-            except NameError:
-                print("Write a number higher than 7")
-
-    def generate(self):
-        """Setiing the password characters"""
-        self.lower = "abcdefghijklmnopqrstuvwxyz"
-        self.upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.digits = "0123456789 !@#$%^&*()"
-        self.string = self.lower + self.upper + self.digits
-        self.password = "".join(random.sample(self.string, self.length))
+# Function for calculation of password
 
 
-if __name__ == "__main__":
-    while True:
-        instance = pass_generator()
-        instance.generate()
-        # Ask the user for run again
-        run_again = input('\nRun Again? [Y/N]')
-        if run_again == 'y' or run_again == 'yes':
-            continue
-        else:
-            os.system('cls')
-            sys.exit()
+def low():
+    entry.delete(0, END)
+
+    # Get the length of password
+    length = var1.get()
+
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digits = "0123456789 !@#$%^&*()"
+    string = lower + upper + digits
+    password = "".join(random.sample(string, length))
+    return password
+
+
+# Function for generation of password
+def generate():
+    password1 = low()
+    entry.insert(10, password1)
+
+
+# Function for copying password to clipboard
+def copy1():
+    random_password = entry.get()
+    pyperclip.copy(random_password)
+
+
+# Main Function
+
+# create GUI window
+root = Tk()
+var = IntVar()
+var1 = IntVar()
+
+# Title of your GUI window
+root.title("Random Password Generator")
+
+# create label and entry to show
+# password generated
+Random_password = Label(root, text="Password")
+Random_password.grid(row=0)
+entry = Entry(root)
+entry.grid(row=0, column=1)
+
+# create label for length of password
+c_label = Label(root, text="Length")
+c_label.grid(row=1)
+
+# create Buttons Copy which will copy
+# password to clipboard and Generate
+# which will generate the password
+copy_button = Button(root, text="Copy", command=copy1)
+copy_button.grid(row=0, column=2)
+generate_button = Button(root, text="Generate", command=generate)
+generate_button.grid(row=0, column=3)
+
+combo = Combobox(root, textvariable=var1)
+
+# Combo Box for length of your password
+combo['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 16,
+                   17, 18, 19, 20, 21, 22, 23, 24, 25,
+                   26, 27, 28, 29, 30, 31, 32)
+combo.current(0)
+combo.bind('<<ComboboxSelected>>')
+combo.grid(column=1, row=1)
+
+# start the GUI
+root.mainloop()
